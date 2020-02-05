41e736bc88fb9bd2acbff59972d5de91ec87d0e7
Joshua Pavoncello <JPavoncello@westfaliausa.com>
2020-02-05 16:33:22 -0500

    Initial commit

---
+++ CsvSplitter/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace CsvSplitter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.fileLbl = new System.Windows.Forms.Label();
+            this.cbColHead = new System.Windows.Forms.CheckBox();
+            this.numPartSizeMB = new System.Windows.Forms.NumericUpDown();
+            this.lblSplits = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.numPartSizeMB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(471, 10);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.button1.Size = new System.Drawing.Size(45, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "•••";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(106, 12);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(359, 20);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // fileLbl
+            // 
+            this.fileLbl.AutoSize = true;
+            this.fileLbl.Location = new System.Drawing.Point(12, 15);
+            this.fileLbl.Name = "fileLbl";
+            this.fileLbl.Size = new System.Drawing.Size(51, 13);
+            this.fileLbl.TabIndex = 2;
+            this.fileLbl.Text = "File Path:";
+            // 
+            // cbColHead
+            // 
+            this.cbColHead.AutoSize = true;
+            this.cbColHead.Location = new System.Drawing.Point(12, 85);
+            this.cbColHead.Name = "cbColHead";
+            this.cbColHead.Size = new System.Drawing.Size(126, 17);
+            this.cbColHead.TabIndex = 3;
+            this.cbColHead.Text = "Has Column Headers";
+            this.cbColHead.UseVisualStyleBackColor = true;
+            // 
+            // numPartSizeMB
+            // 
+            this.numPartSizeMB.Location = new System.Drawing.Point(106, 49);
+            this.numPartSizeMB.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.numPartSizeMB.Name = "numPartSizeMB";
+            this.numPartSizeMB.Size = new System.Drawing.Size(120, 20);
+            this.numPartSizeMB.TabIndex = 4;
+            this.numPartSizeMB.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // lblSplits
+            // 
+            this.lblSplits.AutoSize = true;
+            this.lblSplits.Location = new System.Drawing.Point(12, 51);
+            this.lblSplits.Name = "lblSplits";
+            this.lblSplits.Size = new System.Drawing.Size(93, 13);
+            this.lblSplits.TabIndex = 5;
+            this.lblSplits.Text = "Size Per File (MB):";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(440, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Go";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 120);
+            this.progressBar1.MarqueeAnimationSpeed = 150;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(395, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 155);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblSplits);
+            this.Controls.Add(this.numPartSizeMB);
+            this.Controls.Add(this.cbColHead);
+            this.Controls.Add(this.fileLbl);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "CSV Splitter";
+            ((System.ComponentModel.ISupportInitialize)(this.numPartSizeMB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Label fileLbl;
+        private System.Windows.Forms.CheckBox cbColHead;
+        private System.Windows.Forms.NumericUpDown numPartSizeMB;
+        private System.Windows.Forms.Label lblSplits;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ CsvSplitter/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsvSplitter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.numPartSizeMB.Maximum = Int32.MaxValue;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.txtFilePath.Text = this.openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var filePath = this.txtFilePath.Text;
+
+            if (File.Exists(filePath))
+            {
+                var fileInfo = new FileInfo(filePath);
+
+                var fileSizeInBytes = fileInfo.Length;
+
+                int mbPerFile = Convert.ToInt32(this.numPartSizeMB.Value);
+
+                // Add one to account for error
+                var estNumFilesNeeded = Math.Round(fileSizeInBytes / 1000000.0f / mbPerFile) + 1;
+
+                int lengthOfPaddingNeeded = Convert.ToInt32(Math.Floor(Math.Log10(estNumFilesNeeded))) + 1;
+
+                this.progressBar1.Style = ProgressBarStyle.Marquee;
+
+                Task.Factory.StartNew(() =>
+                {
+                    using (var sr = new StreamReader(filePath))
+                    {
+                        var fileDir = Path.GetDirectoryName(filePath);
+                        var fileName = Path.GetFileName(filePath);
+
+                        string colHeaders = null;
+                        if (this.cbColHead.Checked)
+                        {
+                            colHeaders = sr.ReadLine();
+                        }
+
+                        int fileNum = 0;
+                        var filePartWriter = MakeNewFilePart(fileDir, fileName, fileNum, lengthOfPaddingNeeded, colHeaders);
+
+                        float bytesWrittenToPart = 0;
+
+                        while (!sr.EndOfStream)
+                        {
+                            var line = sr.ReadLine();
+
+                            var lineSizeB = GetAsciiSizeBytes(line);
+
+                            if ((bytesWrittenToPart + lineSizeB) / 1000000.0f >= mbPerFile)
+                            {
+                                fileNum++;
+
+                                filePartWriter.Close();
+
+                                filePartWriter = MakeNewFilePart(fileDir, fileName, fileNum, lengthOfPaddingNeeded, colHeaders);
+
+                                bytesWrittenToPart = 0;
+                                if (colHeaders != null)
+                                {
+                                    bytesWrittenToPart += GetAsciiSizeBytes(colHeaders);
+                                }
+                            }
+
+                            filePartWriter.WriteLine(line);
+
+                            bytesWrittenToPart += lineSizeB;
+                        }
+
+                        filePartWriter.Close();
+
+                        this.Invoke((MethodInvoker) delegate { this.progressBar1.Style = ProgressBarStyle.Continuous; });
+                    }
+                });
+            }
+        }
+
+        private StreamWriter MakeNewFilePart(string fileDir, string fileName, int fileNum, int lengthOfPaddingNeeded, string colHeaders)
+        {
+            var partFilePath = GenerateNewFilePath(fileDir, fileName, fileNum, lengthOfPaddingNeeded);
+
+            var partFileStream = new StreamWriter(File.Open(partFilePath, FileMode.Create, FileAccess.Write), Encoding.ASCII);
+
+            if (colHeaders != null)
+            {
+                partFileStream.WriteLine(colHeaders);
+            }
+
+            return partFileStream;
+        }
+
+        private static string GenerateNewFilePath(string fileDir, string fileName, int fileNum, int lengthOfPaddingNeeded)
+        {
+            return Path.Combine(
+                fileDir,
+                fileName.Substring(0, fileName.Length - 4)
+                    + fileNum.ToString().PadLeft(lengthOfPaddingNeeded, '0')
+                    + ".csv");
+        }
+
+        private static int GetAsciiSizeBytes(string line)
+        {
+            // +2 for the windows line endings \r\n
+            return ASCIIEncoding.ASCII.GetByteCount(line) + 2;
+        }
+    }
+}
